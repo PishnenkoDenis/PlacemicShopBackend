fa5d29e1bba182cb8dd885638498b93076e04cef
Daniil Belocon <d.belocon@dunice.net>
2022-11-24 09:37:59 +0300

    Add login resolvers

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,8 +1,10 @@
-import { Inject } from '@nestjs/common';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Inject, Req, Res } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { serverResponseOK } from 'src/config';
 import { User } from 'src/users/user.model';
 
 import { AuthService } from './auth.service';
+import { LoginViaEmailDto } from './dto/login-via-email.dto';
 import { CreateUserDto } from './dto/registrate-user.dto';
 
 @Resolver(() => User)
@@ -11,6 +13,28 @@ export class AuthResolver {
     @Inject(AuthService)
     private authService: AuthService,
   ) {}
+
+  @Mutation(() => User)
+  async loginUser(
+    @Args('user') user: LoginViaEmailDto,
+    @Res({ passthrough: true }) res: ResponseType,
+  ) {
+    const tokens = await this.authService.login(user);
+    this.authService.setTokens(res, tokens);
+
+    return serverResponseOK;
+  }
+
+  @Query(() => User)
+  async refresh(@Req() req, @Res() res: ResponseType) {
+    const access = req.cookies['access_token'];
+    const refresh = req.cookies['refresh_token'];
+    const tokens = await this.authService.refresh(res, { access, refresh });
+
+    this.authService.setTokens(res, tokens);
+
+    return serverResponseOK;
+  }
 
   @Mutation(() => User)
   async createUser(@Args('dto') dto: CreateUserDto): Promise<string> {
